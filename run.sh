c03ca0884a30b983cc652003f5237540c7e46e14
azuda <aaronzhang587@gmail.com>
2025-03-31 09:08:04 -0600

    add script logging

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-npm install
-node queryDB.js
-python3 output_to_json.py
-node pushToGcal.js
+LOG_FILE = "script.log"
+> "$LOG_FILE"
+echo "Script started at $(date)" >> "$LOG_FILE"
+
+npm install >> "$LOG_FILE" 2>&1
+node queryDB.js >> "$LOG_FILE" 2>&1
+python3 output_to_json.py >> "$LOG_FILE" 2>&1
+node pushToGcal.js >> "$LOG_FILE" 2>&1
+
+echo "Script finished at $(date)" >> "$LOG_FILE"
