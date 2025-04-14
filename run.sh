634e0a9fce722670b368b7a84b766deca998abcb
azuda <aaronzhang587@gmail.com>
2025-04-14 13:26:29 -0600

    add logging for launchd service

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 LOG_DIR="./logs"
 LOG_FILE="$LOG_DIR/$(date '+%Y-%m-%d_%H-%M-%S').log"
 mkdir -p "$LOG_DIR"
-ls -1t "$LOG_DIR" | tail -n +5 | xargs -I {} rm -f "$LOG_DIR/{}"
+ls -1t "$LOG_DIR" | tail -n +7 | xargs -I {} rm -f "$LOG_DIR/{}"
 
 echo "Script started at $(date)" >> "$LOG_FILE"
 
